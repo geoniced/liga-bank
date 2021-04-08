8ca1a2d47b12eb77b9f6762ea7f3144bdae1a89b
Ivan Kashin <barbos1701@gmail.com>
2021-04-08 20:01:50 +0300

    Добавляет сетки для услуг

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -25,50 +25,62 @@ const Services = () => {
       </ul>
 
       <article className="services__service-block service-block">
-        <h3 className="service-block__title">Вклады Лига Банка – это выгодная инвестиция в свое будущее</h3>
-        <ul className="service-block__features">
-          <li className="service-block__features-item">Проценты по вкладам до 7%</li>
-          <li className="service-block__features-item">Разнообразные условия</li>
-          <li className="service-block__features-item">Возможность ежемесячной капитализации или вывод процентов на банковскую карту</li>
-        </ul>
-        <a href="#" className="service-block__more-button button">Узнать подробнее</a>
-
-        <img src={piggyBankImg} alt="Копилка с возрастающими столбиками монет" width="440" height="290" />
+        <div className="service-block__description-column">
+          <h3 className="service-block__title">Вклады Лига Банка – это выгодная инвестиция в свое будущее</h3>
+          <ul className="service-block__features">
+            <li className="service-block__features-item">Проценты по вкладам до 7%</li>
+            <li className="service-block__features-item">Разнообразные условия</li>
+            <li className="service-block__features-item">Возможность ежемесячной капитализации или вывод процентов на банковскую карту</li>
+          </ul>
+          <a href="#" className="service-block__more-button button">Узнать подробнее</a>
+        </div>
+        <div className="service-block__img-column">
+          <img src={piggyBankImg} alt="Копилка с возрастающими столбиками монет" width="440" height="290" />
+        </div>
       </article>
 
       <article className="services__service-block service-block">
-        <h3 className="service-block__title">Лига Банк выдает кредиты под любые цели</h3>
-        <ul className="service-block__features">
-          <li className="service-block__features-item">Ипотечный кредит</li>
-          <li className="service-block__features-item">Автокредит</li>
-          <li className="service-block__features-item">Потребительский кредит</li>
-        </ul>
-        <p className="service-block__description">Рассчитайте ежемесячный платеж и ставку по кредиту воспользовавшись нашим <a className="service-block__link" href="#">кредитным калькулятором</a></p>
-
-        <img src={carOnAPileOfCoinsImg} alt="Автомобиль на стопке монет" width="440" height="290" />
+        <div className="service-block__description-column">
+          <h3 className="service-block__title">Лига Банк выдает кредиты под любые цели</h3>
+          <ul className="service-block__features">
+            <li className="service-block__features-item">Ипотечный кредит</li>
+            <li className="service-block__features-item">Автокредит</li>
+            <li className="service-block__features-item">Потребительский кредит</li>
+          </ul>
+          <p className="service-block__description">Рассчитайте ежемесячный платеж и ставку по кредиту воспользовавшись нашим <a className="service-block__link" href="#">кредитным калькулятором</a></p>
+        </div>
+        <div className="service-block__img-column">
+          <img src={carOnAPileOfCoinsImg} alt="Автомобиль на стопке монет" width="440" height="290" />
+        </div>
       </article>
 
       <article className="services__service-block service-block">
-        <h3 className="service-block__title">Лига Страхование — застрахуем все что захотите</h3>
-        <ul className="service-block__features">
-          <li className="service-block__features-item">Автомобильное страхование</li>
-          <li className="service-block__features-item">Страхование жизни и здоровья</li>
-          <li className="service-block__features-item">Страхование недвижимости</li>
-        </ul>
-        <a href="#" className="service-block__more-button button">Узнать подробнее</a>
-
-        <img src={heartShapedLockImg} alt="Замок с иконкой сердечка на нем" width="440" height="290" />
+        <div className="service-block__description-column">
+          <h3 className="service-block__title">Лига Страхование — застрахуем все что захотите</h3>
+          <ul className="service-block__features">
+            <li className="service-block__features-item">Автомобильное страхование</li>
+            <li className="service-block__features-item">Страхование жизни и здоровья</li>
+            <li className="service-block__features-item">Страхование недвижимости</li>
+          </ul>
+          <a href="#" className="service-block__more-button button">Узнать подробнее</a>
+        </div>
+        <div className="service-block__img-column">
+          <img src={heartShapedLockImg} alt="Замок с иконкой сердечка на нем" width="440" height="290" />
+        </div>
       </article>
 
       <article className="services__service-block service-block">
-        <h3 className="service-block__title">Лига Банк — это огромное количество онлайн-сервисов для вашего удобства</h3>
-        <ul className="service-block__features">
-          <li className="service-block__features-item">Мобильный банк, который всегда под рукой</li>
-          <li className="service-block__features-item">Приложение Лига-проездной позволит вам оплачивать билеты по всему миру</li>
-        </ul>
-        <a href="#" className="service-block__more-button button">Узнать подробнее</a>
-
-        <img src={ligaBankOnMobileImg} alt="Приложение Лига-Банка на телефоне" width="440" height="290" />
+        <div className="service-block__description-column">
+          <h3 className="service-block__title">Лига Банк — это огромное количество онлайн-сервисов для вашего удобства</h3>
+          <ul className="service-block__features">
+            <li className="service-block__features-item">Мобильный банк, который всегда под рукой</li>
+            <li className="service-block__features-item">Приложение Лига-проездной позволит вам оплачивать билеты по всему миру</li>
+          </ul>
+          <a href="#" className="service-block__more-button button">Узнать подробнее</a>
+        </div>
+        <div className="service-block__img-column">
+          <img src={ligaBankOnMobileImg} alt="Приложение Лига-Банка на телефоне" width="440" height="290" />
+        </div>
       </article>
     </section>
   );
