71db6c933b3911ed20e462af2cfe98c36907ffe4
Ivan Kashin <barbos1701@gmail.com>
2021-04-09 20:39:54 +0300

    Добавляет декоративные элементы для блока отделений

--- src/components/bank-departments/bank-departments.jsx
+++ src/components/bank-departments/bank-departments.jsx
@@ -1,11 +1,20 @@
 import departmentsMapImg from "../../assets/img/departments-map.jpg";
+import {ReactComponent as IconLocationBlip} from "../../assets/img/icon-location-blip.svg";
 
 const BankDepartments = () => {
   return (
     <section className="page-content__bank-departments bank-departments">
       <h2 className="bank-departments__title">Отделения Лига Банка</h2>
 
-      <img className="bank-departments__map" src={departmentsMapImg} alt="Расположение отделений Лига Банков" width="1170" height="462" />
+      <div className="bank-departments__map-wrapper">
+        <img className="bank-departments__map" src={departmentsMapImg} alt="Расположение отделений Лига Банков" width="1170" height="462" />
+
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--moscow"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--saratov"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--kazan"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--tyumen"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--omsk"/>
+      </div>
     </section>
   );
 };
