cd952f273f017a037fcabfb89c67e16ca3234cba
Ivan Kashin <barbos1701@gmail.com>
2021-04-11 12:52:09 +0300

    Делает адаптив для отделений на планшете

--- src/components/bank-departments/bank-departments.jsx
+++ src/components/bank-departments/bank-departments.jsx
@@ -1,4 +1,5 @@
 import departmentsMapImg from "../../assets/img/departments-map.jpg";
+import departmentsMapImgTablet from "../../assets/img/departments-map-tablet.jpg";
 import {ReactComponent as IconLocationBlip} from "../../assets/img/icon-location-blip.svg";
 
 const BankDepartments = () => {
@@ -7,13 +8,18 @@ const BankDepartments = () => {
       <h2 className="bank-departments__title">Отделения Лига Банка</h2>
 
       <div className="bank-departments__map-wrapper">
-        <img className="bank-departments__map" src={departmentsMapImg} alt="Расположение отделений Лига Банков" width="1170" height="462" />
-
+        <picture className="bank-departments__picture-container">
+          {/* <source media="(max-width: 767px)" srcset="img/logo-mobile.svg" /> */}
+          <source media="(max-width: 1023px)" srcSet={departmentsMapImgTablet} />
+          <img className="bank-departments__map" src={departmentsMapImg} alt="Расположение отделений Лига Банков" width="1170" height="462" />
+        </picture>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--moscow"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--saratov"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--kazan"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--tyumen"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--omsk"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--surgut"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--perm"/>
       </div>
     </section>
   );
