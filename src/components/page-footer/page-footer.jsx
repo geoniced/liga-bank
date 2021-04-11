d5382c11cc9d73f1fe27bfd0802e63c7dc901bf9
Ivan Kashin <barbos1701@gmail.com>
2021-04-11 11:39:45 +0300

    Делает адаптив для футера

--- src/components/page-footer/page-footer.jsx
+++ src/components/page-footer/page-footer.jsx
@@ -15,11 +15,13 @@ const PageFooter = () => {
             <img className="logo__image" src={logo} alt="Логотип сайта Лига Банк" width="149" height="25" />
           </a>
 
-          <address className="contacts-navigation__address">150015, г. Москва, ул. Московская, д. 32</address>
-          <p className="contacts-navigation__copyright">
-            Генеральная лицензия Банка России №1050
-            Ⓒ Лига Банк, 2019
-          </p>
+          <div className="contacts-navigation__information">
+            <address className="contacts-navigation__address">150015, г. Москва, ул. Московская, д. 32</address>
+            <p className="contacts-navigation__copyright">
+              Генеральная лицензия Банка России №1050
+              Ⓒ Лига Банк, 2019
+            </p>
+          </div>
 
           <ul className="contacts-navigation__site-navigation site-navigation site-navigation--vertical">
             <li className="site-navigation__item">
@@ -36,52 +38,51 @@ const PageFooter = () => {
             </li>
           </ul>
         </div>
-        {/* <div className="page-footer__contact-address contact-address">
 
-        </div> */}
+        <div className="page-footer__phones-and-socials">
+          <div className="page-footer__phone-contacts page-footer__phone-contacts--mobile">
+            <a href="tel:*0904" className="page-footer__phone">
+              <IconSmartPhone className="page-footer__mobile-phone-icon" />
+              <span className="page-footer__phone-number">*0904</span>
+            </a>
+            <p className="page-footer__phone-description">Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2</p>
+          </div>
 
-        <div className="page-footer__phone-contacts page-footer__phone-contacts--mobile">
-          <a href="tel:*0904" className="page-footer__phone">
-            <IconSmartPhone className="page-footer__mobile-phone-icon" />
-            <span className="page-footer__phone-number">*0904</span>
-          </a>
-          <p className="page-footer__phone-description">Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2</p>
+          <div className="page-footer__phone-contacts page-footer__phone-contacts--base">
+            <a href="tel:88001112233" className="page-footer__phone">
+              <IconBasePhone className="page-footer__base-phone-icon" />
+              <span className="page-footer__phone-number">8 800 111 22 33</span>
+            </a>
+            <p className="page-footer__phone-description">Бесплатный для всех городов России</p>
+          </div>
+
+          <ul className="page-footer__socials socials">
+            <li className="socials__item">
+              <a href="#" className="socials__link">
+                <IconFacebook className="socials__icon socials__icon--fb"/>
+                <span className="visually-hidden">Facebook</span>
+              </a>
+            </li>
+            <li className="socials__item">
+              <a href="#" className="socials__link">
+                <IconInstagram className="socials__icon socials__icon--instagram"/>
+                <span className="visually-hidden">Instagram</span>
+              </a>
+            </li>
+            <li className="socials__item">
+              <a href="#" className="socials__link">
+                <IconTwitter className="socials__icon socials__icon--twitter"/>
+                <span className="visually-hidden">Twitter</span>
+              </a>
+            </li>
+            <li className="socials__item">
+              <a href="#" className="socials__link">
+                <IconYoutube className="socials__icon socials__icon--yt"/>
+                <span className="visually-hidden">YouTube</span>
+              </a>
+            </li>
+          </ul>
         </div>
-
-        <div className="page-footer__phone-contacts page-footer__phone-contacts--base">
-          <a href="tel:88001112233" className="page-footer__phone">
-            <IconBasePhone className="page-footer__base-phone-icon" />
-            <span className="page-footer__phone-number">8 800 111 22 33</span>
-          </a>
-          <p className="page-footer__phone-description">Бесплатный для всех городов России</p>
-        </div>
-
-        <ul className="page-footer__socials socials">
-          <li className="socials__item">
-            <a href="#" className="socials__link">
-              <IconFacebook className="socials__icon socials__icon--fb"/>
-              <span className="visually-hidden">Facebook</span>
-            </a>
-          </li>
-          <li className="socials__item">
-            <a href="#" className="socials__link">
-              <IconInstagram className="socials__icon socials__icon--instagram"/>
-              <span className="visually-hidden">Instagram</span>
-            </a>
-          </li>
-          <li className="socials__item">
-            <a href="#" className="socials__link">
-              <IconTwitter className="socials__icon socials__icon--twitter"/>
-              <span className="visually-hidden">Twitter</span>
-            </a>
-          </li>
-          <li className="socials__item">
-            <a href="#" className="socials__link">
-              <IconYoutube className="socials__icon socials__icon--yt"/>
-              <span className="visually-hidden">YouTube</span>
-            </a>
-          </li>
-        </ul>
       </div>
     </footer>
   );
