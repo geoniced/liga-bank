93283bd9b6d00735591b9ca4dcde2a71bc1faced
Ivan Kashin <barbos1701@gmail.com>
2021-04-13 16:23:15 +0300

    Оборачивает контент слайдера в центруемый враппер

--- src/components/promo-slider/promo-slider.jsx
+++ src/components/promo-slider/promo-slider.jsx
@@ -6,18 +6,27 @@ const PromoSlider = () => {
       {/* <!-- TODO: посмотреть как слайдеры я делал ранее --> */}
       <ul className="promo-slider__list">
         <li className="promo-slider__item promo-slider__item--credit">
-          <h2 className="promo-slider__title">Лига Банк</h2>
-          <p className="promo-slider__description">Кредиты на любой случай</p>
-          <a href="#" className="promo-slider__promo-button button button--white">Рассчитать кредит</a>
+          <div className="promo-slider__wrapper">
+            <h2 className="promo-slider__title">Лига Банк</h2>
+            <p className="promo-slider__description">Кредиты на любой случай</p>
+            <a href="#" className="promo-slider__promo-button button button--white">Рассчитать кредит</a>
+          </div>
+
         </li>
         <li className="promo-slider__item promo-slider__item--confidence">
-          <h2 className="promo-slider__title">Лига Банк</h2>
-          <p className="promo-slider__description">Ваша уверенность в завтрашнем дне</p>
+          <div className="promo-slider__wrapper">
+            <h2 className="promo-slider__title">Лига Банк</h2>
+            <p className="promo-slider__description">Ваша уверенность в завтрашнем дне</p>
+          </div>
+
         </li>
         <li className="promo-slider__item promo-slider__item--department">
-          <h2 className="promo-slider__title">Лига Банк</h2>
-          <p className="promo-slider__description">Всегда рядом</p>
-          <a href="#" className="promo-slider__promo-button promo-slider__promo-button--department button">Найти отделение</a>
+          <div className="promo-slider__wrapper">
+            <h2 className="promo-slider__title">Лига Банк</h2>
+            <p className="promo-slider__description">Всегда рядом</p>
+            <a href="#" className="promo-slider__promo-button promo-slider__promo-button--department button">Найти отделение</a>
+          </div>
+
         </li>
       </ul>
 
