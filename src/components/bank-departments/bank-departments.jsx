f05be9815e482b598d88b32d0d0b68518800fe29
Ivan Kashin <barbos1701@gmail.com>
2021-04-14 12:55:03 +0300

    Делает резину на десктопе для отделений банка, убирает иконки, заменяет на карту отделений

--- src/components/bank-departments/bank-departments.jsx
+++ src/components/bank-departments/bank-departments.jsx
@@ -1,7 +1,6 @@
-import departmentsMapImg from "../../assets/img/departments-map.jpg";
-import departmentsMapImgTablet from "../../assets/img/departments-map-tablet.jpg";
-import departmentsMapImgMobile from "../../assets/img/departments-map-mobile.jpg";
-import {ReactComponent as IconLocationBlip} from "../../assets/img/icon-location-blip.svg";
+import departmentsMapImg from "../../assets/img/deps-map-with-blips.jpg";
+import departmentsMapImgTablet from "../../assets/img/deps-map-with-blips-tablet.jpg";
+import departmentsMapImgMobile from "../../assets/img/deps-map-with-blips-mobile.jpg";
 
 const BankDepartments = () => {
   return (
@@ -14,14 +13,14 @@ const BankDepartments = () => {
           <source media="(max-width: 1023px)" srcSet={departmentsMapImgTablet} />
           <img className="bank-departments__map" src={departmentsMapImg} alt="Расположение отделений Лига Банков" width="1170" height="462" />
         </picture>
-        <IconLocationBlip className="bank-departments__blip bank-departments__blip--moscow"/>
+        {/* <IconLocationBlip className="bank-departments__blip bank-departments__blip--moscow"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--saratov"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--kazan"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--tyumen"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--omsk"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--surgut"/>
         <IconLocationBlip className="bank-departments__blip bank-departments__blip--perm"/>
-        <IconLocationBlip className="bank-departments__blip bank-departments__blip--novosib"/>
+        <IconLocationBlip className="bank-departments__blip bank-departments__blip--novosib"/> */}
       </div>
     </section>
   );
