058d7208d0dda1739ff1bc5175420f283369093b
Ivan Kashin <barbos1701@gmail.com>
2021-04-16 16:01:31 +0300

    Делает рефакторинг слайдов

--- src/components/promo-slider/promo-slider.jsx
+++ src/components/promo-slider/promo-slider.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {ReactComponent as IconSliderItem} from "../../assets/img/icon-slider-item.svg";
 import {SLIDER_CHANGE_INTERVAL} from "../../const";
+import PromoSliderItem from "../promo-slider-item/promo-slider-item";
+import SliderButtonItem from "../slider-button-item/slider-button-item";
 
 const PromoSlider = () => {
   return (
@@ -16,38 +17,28 @@ const PromoSlider = () => {
         autoPlay
         infiniteLoop
         renderIndicator={(onClickHandler, isSelected, index) => (
-          <li
-            onClick={onClickHandler}
-            className={`slider-buttons__item ${isSelected ? `slider-buttons__item--active` : ``}`}
-          >
-            <button className="slider-buttons__button" type="button">
-              <IconSliderItem className="slider-buttons__icon" />
-              <span className="visually-hidden">{index + 1} слайд</span>
-            </button>
-          </li>
+          <SliderButtonItem
+            onClickHandler={onClickHandler}
+            isSelected={isSelected}
+            index={index}
+          />
         )}
         className="promo-slider__list slider-buttons slider-buttons--promo-slider"
       >
-        <div className="promo-slider__item promo-slider__item--credit">
-          <div className="promo-slider__wrapper">
-            <h2 className="promo-slider__title">Лига Банк</h2>
-            <p className="promo-slider__description">Кредиты на любой случай</p>
-            <a href="#" className="promo-slider__promo-button button button--white">Рассчитать кредит</a>
-          </div>
-        </div>
-        <div className="promo-slider__item promo-slider__item--confidence">
-          <div className="promo-slider__wrapper">
-            <h2 className="promo-slider__title">Лига Банк</h2>
-            <p className="promo-slider__description">Ваша уверенность в завтрашнем дне</p>
-          </div>
-        </div>
-        <div className="promo-slider__item promo-slider__item--department">
-          <div className="promo-slider__wrapper">
-            <h2 className="promo-slider__title">Лига Банк</h2>
-            <p className="promo-slider__description">Всегда рядом</p>
-            <a href="#" className="promo-slider__promo-button promo-slider__promo-button--department button">Найти отделение</a>
-          </div>
-        </div>
+        <PromoSliderItem itemModifierClass="credit">
+          <h2 className="promo-slider__title">Лига Банк</h2>
+          <p className="promo-slider__description">Кредиты на любой случай</p>
+          <a href="#" className="promo-slider__promo-button button button--white">Рассчитать кредит</a>
+        </PromoSliderItem>
+        <PromoSliderItem itemModifierClass="confidence">
+          <h2 className="promo-slider__title">Лига Банк</h2>
+          <p className="promo-slider__description">Ваша уверенность в завтрашнем дне</p>
+        </PromoSliderItem>
+        <PromoSliderItem itemModifierClass="department">
+          <h2 className="promo-slider__title">Лига Банк</h2>
+          <p className="promo-slider__description">Всегда рядом</p>
+          <a href="#" className="promo-slider__promo-button promo-slider__promo-button--department button">Найти отделение</a>
+        </PromoSliderItem>
       </Carousel>
     </section>
   );
