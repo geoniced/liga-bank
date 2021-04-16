87d8e9d2a0cd60e4af47ef8abbf0b16788dd388f
Ivan Kashin <barbos1701@gmail.com>
2021-04-16 16:12:22 +0300

    Делает рефактор промослайдов

--- src/components/promo-slider/promo-slider.jsx
+++ src/components/promo-slider/promo-slider.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {SLIDER_CHANGE_INTERVAL} from "../../const";
-import PromoSliderItem from "../promo-slider-item/promo-slider-item";
+import PromoConfidence from "../promo-confidence/promo-confidence";
+import PromoCredit from "../promo-credit/promo-credit";
+import PromoDepartment from "../promo-department/promo-department";
 import SliderButtonItem from "../slider-button-item/slider-button-item";
 
 const PromoSlider = () => {
@@ -25,20 +27,9 @@ const PromoSlider = () => {
         )}
         className="promo-slider__list slider-buttons slider-buttons--promo-slider"
       >
-        <PromoSliderItem itemModifierClass="credit">
-          <h2 className="promo-slider__title">Лига Банк</h2>
-          <p className="promo-slider__description">Кредиты на любой случай</p>
-          <a href="#credit-calculator" className="promo-slider__promo-button button button--white">Рассчитать кредит</a>
-        </PromoSliderItem>
-        <PromoSliderItem itemModifierClass="confidence">
-          <h2 className="promo-slider__title">Лига Банк</h2>
-          <p className="promo-slider__description">Ваша уверенность в завтрашнем дне</p>
-        </PromoSliderItem>
-        <PromoSliderItem itemModifierClass="department">
-          <h2 className="promo-slider__title">Лига Банк</h2>
-          <p className="promo-slider__description">Всегда рядом</p>
-          <a href="#bank-departments" className="promo-slider__promo-button promo-slider__promo-button--department button">Найти отделение</a>
-        </PromoSliderItem>
+        <PromoCredit />
+        <PromoConfidence />
+        <PromoDepartment />
       </Carousel>
     </section>
   );
