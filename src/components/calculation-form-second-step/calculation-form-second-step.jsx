a393856f3125c3610a8cf6cbe5f6595b7497bc2e
Ivan Kashin <barbos1701@gmail.com>
2021-04-18 09:38:29 +0300

    Рефакторит вынося все лишнее из компонента числового поля

--- src/components/calculation-form-second-step/calculation-form-second-step.jsx
+++ src/components/calculation-form-second-step/calculation-form-second-step.jsx
@@ -4,6 +4,8 @@ import CalculationFormRange from "../calculation-form-range/calculation-form-ran
 import {CreditStep, CREDIT_DEFAULT_COST} from "../../const";
 import NumericField from "../numeric-field/numeric-field";
 import {formatDecimal, formatDecimalWithRubles} from "../../utils";
+import {ReactComponent as IconMinus} from "../../assets/img/icon-minus.svg";
+import {ReactComponent as IconPlus} from "../../assets/img/icon-plus.svg";
 
 const CalculationFormSecondStep = (props) => {
   const {
@@ -39,16 +41,26 @@ const CalculationFormSecondStep = (props) => {
 
       <div className="calculation-form__input-field-row">
         <label htmlFor="calculation-form-property-cost" className="calculation-form__label">{creditInfo.creditName}</label>
-        <NumericField
-          min={creditInfo.cost.min}
-          max={creditInfo.cost.max}
-          step={creditInfo.cost.step}
-          onChange={onPropertyCostChange}
-          onOperationMinusClick={onOperationMinusClick}
-          onOperationPlusClick={onOperationPlusClick}
-          convertCallback={formatDecimalWithRubles}
-          value={creditPropertyCost}
-        />
+        <div className="calculation-form__input-with-operations">
+          <NumericField
+            name="calculation-form-property-cost"
+            min={creditInfo.cost.min}
+            max={creditInfo.cost.max}
+            step={creditInfo.cost.step}
+            onChange={onPropertyCostChange}
+            convertCallback={formatDecimalWithRubles}
+            value={creditPropertyCost}
+          />
+
+          <button onClick={onOperationMinusClick} className="calculation-form__operation-button calculation-form__operation-button--minus" type="button">
+            <IconMinus className="calculation-form__operation-minus"/>
+            <span className="visually-hidden">Уменьшить</span>
+          </button>
+          <button onClick={onOperationPlusClick} className="calculation-form__operation-button calculation-form__operation-button--plus" type="button">
+            <IconPlus className="calculation-form__operation-plus"/>
+            <span className="visually-hidden">Увеличить</span>
+          </button>
+        </div>
         <p className="calculation-form__field-description">От {formatDecimal(creditInfo.cost.min)} ​​&nbsp;до {formatDecimal(creditInfo.cost.max)} рублей</p>
       </div>
 
--- src/components/numeric-field/numeric-field.jsx
+++ src/components/numeric-field/numeric-field.jsx
@@ -1,18 +1,15 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
-import {ReactComponent as IconMinus} from "../../assets/img/icon-minus.svg";
-import {ReactComponent as IconPlus} from "../../assets/img/icon-plus.svg";
 
 const NumericField = (props) => {
   const {
+    name,
     min,
     max,
     step,
     onChange,
     value,
     convertCallback,
-    onOperationMinusClick,
-    onOperationPlusClick,
   } = props;
 
   const [isEditing, setIsEditing] = useState(false);
@@ -22,13 +19,13 @@ const NumericField = (props) => {
   };
 
   return (
-    <div className="calculation-form__input-with-operations">
+    <>
       {isEditing ? (
         <input
           className="calculation-form__input calculation-form__input--number"
           type="number"
-          name="calculation-form-property-cost"
-          id="calculation-form-property-cost"
+          name={name}
+          id={name}
           min={min}
           max={max}
           step={step}
@@ -40,23 +37,14 @@ const NumericField = (props) => {
         <input
           className="calculation-form__input calculation-form__input--number"
           type="text"
-          name="calculation-form-property-cost"
-          id="calculation-form-property-cost"
+          name={name}
+          id={name}
           onFocus={toggleEditable}
           value={convertCallback(value)}
           readOnly
         />
       )}
-
-      <button onClick={onOperationMinusClick} className="calculation-form__operation-button calculation-form__operation-button--minus" type="button">
-        <IconMinus className="calculation-form__operation-minus"/>
-        <span className="visually-hidden">Уменьшить</span>
-      </button>
-      <button onClick={onOperationPlusClick} className="calculation-form__operation-button calculation-form__operation-button--plus" type="button">
-        <IconPlus className="calculation-form__operation-plus"/>
-        <span className="visually-hidden">Увеличить</span>
-      </button>
-    </div>
+    </>
   );
 };
 
